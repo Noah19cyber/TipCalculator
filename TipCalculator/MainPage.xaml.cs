93d89e00a927054f664fb7cb03cf18ca5f8d6f52
Noah19cyber <alineborgescunha.abc@gmail.com>
2024-08-23 21:44:01 -0300

    Aula mobile

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setLowTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setHighTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+
